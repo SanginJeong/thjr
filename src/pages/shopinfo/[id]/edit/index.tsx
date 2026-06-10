92bc2e9dcde2b5dbf23cf6b8697fa768be2e1a88
SanginJeong <sanginjeong07@gmail.com>
2026-06-10 13:50:53 +0900

    refactor: shop SSR -> CSR 변경

--- src/pages/shopinfo/[id]/edit/index.tsx
+++ src/pages/shopinfo/[id]/edit/index.tsx
@@ -1,40 +1,22 @@
 import Head from "next/head";
-import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { dehydrate, QueryClient, useQueryClient } from "@tanstack/react-query";
-import { getShopInfo, useGetShopInfoQuery } from "@/hooks/api/shop/useGetShopInfoQuery";
+import { useGetShopInfoQuery } from "@/hooks/api/shop/useGetShopInfoQuery";
 import { usePutShopInfoQuery } from "@/hooks/api/shop/usePutShopInfoQuery";
 import RegisterForm, { FormData } from "@/pages/shopinfo/_components/RegisterForm";
 import { useEffect } from "react";
 import IcClose from "@/assets/svgs/ic_close.svg";
 import Layout from "@/components/Layout";
 import { useRouter } from "next/router";
-import { getCookieValue } from "@/utils/getCookie";
-import { checkAuthSSR } from "@/utils/checkAuth";
 import { useModal } from "@/hooks/useModal";
 import SkeletonUI from "@/components/Skeleton";
+import { QueryClient } from "@tanstack/react-query";
+import { useAuth } from "@/hooks/useAuth";
 
-export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-  const { redirect } = checkAuthSSR(context, "employer", true);
-  if (redirect) {
-    return { redirect };
-  }
-  const cookie = context.req.headers.cookie;
-  const userId = getCookieValue(cookie, "userId") || "";
-  const shopId = getCookieValue(cookie, "shopId") || "";
-
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["getShopInfo", shopId],
-    queryFn: () => getShopInfo(shopId),
-  });
-
-  return { props: { userId, shopId, dehydratedState: dehydrate(queryClient) } };
-};
-
-const EditShopPage = ({ userId, shopId }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const EditShopPage = () => {
   const router = useRouter();
-  const queryClient = useQueryClient();
-
+  const { id } = router.query;
+  const { userId } = useAuth();
+  const shopId = String(id);
+  const queryClient = new QueryClient();
   const { data: shopData, isPending: isGetPending } = useGetShopInfoQuery(shopId);
   const { mutate: updateShop, isPending: isPutPending } = usePutShopInfoQuery();
 
@@ -108,22 +90,22 @@ const EditShopPage = ({ userId, shopId }: InferGetServerSidePropsType<typeof get
         <meta name="description" content="가게 정보를 수정하세요." />
       </Head>
       <div className="bg-gray-5">
-      <div className="m-auto max-w-1028 px-12 py-40 tablet:px-32 tablet:py-60">
-        <div className="relative">
-          <IcClose
-            onClick={handleCloseClick}
-            className="absolute right-0 top-0 w-24 hover:cursor-pointer tablet:w-32"
-          />
-          <h1 className="mb-32 text-20-bold text-black tablet:text-28-bold">가게 수정</h1>
-          <RegisterForm
-            defaultValues={defaultValues}
-            onSubmit={handleSubmit}
-            isPending={isPutPending}
-            submitLabel="수정"
-          />
+        <div className="m-auto max-w-1028 px-12 py-40 tablet:px-32 tablet:py-60">
+          <div className="relative">
+            <IcClose
+              onClick={handleCloseClick}
+              className="absolute right-0 top-0 w-24 hover:cursor-pointer tablet:w-32"
+            />
+            <h1 className="mb-32 text-20-bold text-black tablet:text-28-bold">가게 수정</h1>
+            <RegisterForm
+              defaultValues={defaultValues}
+              onSubmit={handleSubmit}
+              isPending={isPutPending}
+              submitLabel="수정"
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
